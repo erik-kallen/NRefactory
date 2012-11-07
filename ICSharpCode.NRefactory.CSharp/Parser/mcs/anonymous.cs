daed6f23c032e387b6368a6f426660dbb9bd7974
Erik Källén <erik_kallen@hotmail.com>
2012-11-07 16:32:59 +0100

    Revert "Ugly patch to mcs to prevent internal errors (https://bugzilla.xamarin.com/show_bug.cgi?id=7826)"
    This reverts commit fe07f0d59294bd6015398a880230bbd76b37d2f4.

--- ICSharpCode.NRefactory.CSharp/Parser/mcs/anonymous.cs
+++ ICSharpCode.NRefactory.CSharp/Parser/mcs/anonymous.cs
@@ -572,16 +572,13 @@ namespace Mono.CSharp {
 				//
 				// Parameters could be proxied via local fields for value type storey
 				//
-
-// Code commented out to fix #77. Reported upstream: https://bugzilla.xamarin.com/show_bug.cgi?id=7826
-
-//				if (hoisted_local_params != null) {
-//					var local_param = hoisted_local_params.Find (l => l.Parameter.Parameter == hp.Parameter.Parameter);
-//					var source = new FieldExpr (local_param.Field, Location);
-//					source.InstanceExpression = new CompilerGeneratedThis (CurrentType, Location);
-//					hp.EmitAssign (ec, source, false, false);
-//					continue;
-//				}
+				if (hoisted_local_params != null) {
+					var local_param = hoisted_local_params.Find (l => l.Parameter.Parameter == hp.Parameter.Parameter);
+					var source = new FieldExpr (local_param.Field, Location);
+					source.InstanceExpression = new CompilerGeneratedThis (CurrentType, Location);
+					hp.EmitAssign (ec, source, false, false);
+					continue;
+				}
 
 				hp.EmitHoistingAssignment (ec);
 			}
